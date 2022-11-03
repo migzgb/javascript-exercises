c166e9c826fec788d0fba7069f3fa50ec6ca0250
migzgb <miguel.gibhran.pana@gmail.com>
2022-11-03 22:59:48 +0800

    finished lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(removeFrom, ...args) {
+    //remove values from array using splice method
+    for (a = 0; a < args.length; a++) {
+        for (var i = 0; i < removeFrom.length; i++) {
+            if (removeFrom[i] == args[a] &&
+                typeof removeFrom[i] == typeof args[a]
+                ) {
+                removeFrom.splice(i,1);
+            }
+        }
+    }
+    return removeFrom
 };
 
 // Do not edit below this line
