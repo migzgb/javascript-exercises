d9ca8479b8db4e6fe2fbf29d03ede2cf461ab0ba
migzgb <miguel.gibhran.pana@gmail.com>
2022-11-03 22:18:43 +0800

    finished lessons 1,2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function( str, times) {
+    newStr = "";
+    if (times < 0) {
+        return "ERROR"
+    }
 
+    else if (times == 0) {
+        return newStr
+    }
+
+    else {
+        for (let i = 0; i < times; i++) {
+            newStr += str;
+        }
+        return newStr
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    finalStr = "";
+    //split str into array
+    let splitStr = str.split('');
+    //concatenate array starting from the last element
+    for  (let i = splitStr.length - 1; i >= 0; i--) {
+        finalStr += splitStr[i];
+    }
 
+    //return new concatenated str
+    return finalStr
 };
 
 // Do not edit below this line
