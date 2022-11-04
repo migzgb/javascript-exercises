095ee2cb24fe3da7b2b12142b339538cc967a2db
migzgb <miguel.gibhran.pana@gmail.com>
2022-11-04 17:34:42 +0800

    finished lesson 6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let n = 5 / 9 * (far-32)
+  let round = Math.round(n * 10) / 10;
+  return round
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let n = 9 / 5 * cel + 32;
+  let round = Math.round(n * 10) / 10;
+  return round
 };
 
 // Do not edit below this line
