e2c477600b0704f4e0a87abe2bb0bf4f490ee1e7
migzgb <miguel.gibhran.pana@gmail.com>
2022-11-04 16:39:24 +0800

    finished lesson 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    //century years computation
+    if (year % 100 == 0 && year % 400 == 0) {
+        return true
+    }
+    
+    // non century years computation
+    else if ( year % 100 != 0 && year % 4 == 0) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
