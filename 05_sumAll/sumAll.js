d3f0346e2a3196f2ec99328ba9120d88bd84b267
migzgb <miguel.gibhran.pana@gmail.com>
2022-11-04 16:21:36 +0800

    finished lesson 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let finalSum = 0;
+    let smallerNum;
+    let largerNum;
+    //check which one is smaller or bigger
+    if (x <= y) {
+        smallerNum = x;
+        largerNum = y;
+    }
+    else {
+        smallerNum = y;
+        largerNum = x;
+    }
 
+    // sum proper
+    if (x < 0 || 
+        y < 0 || 
+        typeof x != "number" || 
+        typeof y != "number" ) {
+        return "ERROR"
+    }
+
+    else {
+        for (i = smallerNum; i < largerNum+1 ; i++) {
+            finalSum += i;
+        }
+        return finalSum
+    }
 };
 
 // Do not edit below this line
